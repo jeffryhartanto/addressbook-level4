a8188c281dcfefb1edc9750dc5f3f918c7571c9c
Jun Wen <JunWen.91@gmail.com>
2016-11-05 12:07:44 +0800

    Import Test Case

---
+++ src/test/java/guitests/ImportCommandTest.java
@@ -0,0 +1,21 @@
+package guitests;
+
+import static seedu.taskscheduler.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import org.junit.Test;
+
+import seedu.taskscheduler.logic.commands.ImportCommand;
+
+//@@author A0138696L
+
+public class ImportCommandTest extends TaskSchedulerGuiTest {
+
+    @Test
+    public void setPath() {
+        
+        // Checking for invalid importing filepath before executing import command
+        String importingPath = "datatest/testing";
+        commandBox.runCommand("import " + importingPath);
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ImportCommand.MESSAGE_USAGE_INVALID));
+    }
+}
